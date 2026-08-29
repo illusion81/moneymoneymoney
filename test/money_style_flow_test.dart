b85bf48cd0eee16f5e51ed7c9cc07c2db1027045
Angus M. <angus.maholam@gmail.com>
2026-08-29 16:18:43 +1000

    test: cover resumed money style position

--- test/money_style_flow_test.dart
+++ test/money_style_flow_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:moneymoneymoney/models/money_style.dart';
 import 'package:moneymoneymoney/screens/money_style_flow.dart';
+import 'package:moneymoneymoney/screens/money_style_quiz_screen.dart';
 
 void main() {
   testWidgets('existing session offers resume and start over', (tester) async {
@@ -14,5 +15,10 @@ void main() {
     expect(find.text('Resume'), findsOneWidget);
     await tester.tap(find.text('Start over'));
     expect(cleared, isTrue);
+  });
+  testWidgets('resumed session opens at first unanswered question', (tester) async {
+    final session = AnswerSession(userId: 'u', sessionId: 's', selectedAnswers: {1: 0}, skippedQuestions: {2});
+    await tester.pumpWidget(MaterialApp(home: MoneyStyleQuizScreen(userId: 'u', initialSession: session, onComplete: (_) {})));
+    expect(find.text('3 of 12'), findsOneWidget);
   });
 }
